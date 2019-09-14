89b6333d0d8c09599ec7f93d31def6d5d444625f
Rajat0023 <rajatsingh2237@gmail.com>
2019-09-14 21:21:26 +0530

    Added check for Contact number already exists, SGR-001

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CustomerBusinessService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CustomerBusinessService.java
@@ -1,6 +1,8 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
 import com.upgrad.FoodOrderingApp.service.dao.CustomerDao;
+import com.upgrad.FoodOrderingApp.service.entity.Customer;
+import com.upgrad.FoodOrderingApp.service.exception.SignUpRestrictedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -18,7 +20,12 @@ public class CustomerBusinessService {
     @Autowired
     private PasswordCryptographyProvider passwordCryptographyProvider;
 
+  public Customer signUp(Customer customer)
+          throws SignUpRestrictedException {
 
-
+    if (customer.getContactNumber()!= null && customerDao.getContactNumber(customer.getContactNumber()) != null) {
+      throw new SignUpRestrictedException(
+              "SGR-001", "This contact number is already registered! Try other contact number.");
+    }
 
 }
--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CustomerDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CustomerDao.java
@@ -1,11 +1,29 @@
 package com.upgrad.FoodOrderingApp.service.dao;
 
 
+import com.upgrad.FoodOrderingApp.service.entity.Customer;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
 
 
 @Repository
 public class CustomerDao {
+    @PersistenceContext
+    private EntityManager entityManager;
 
+    public Customer getContactNumber(String contactNumber){
 
+        try {
+            return entityManager.createNamedQuery("findContactNumber", Customer.class)
+                    .setParameter("contactNumber",contactNumber)
+                    .getSingleResult();
+        }
+        catch (NoResultException nre) {
+            return null;
+        }
+
+    }
 }
