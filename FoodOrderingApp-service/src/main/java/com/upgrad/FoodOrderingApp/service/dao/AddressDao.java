1b024e538a8f6be55cbd41aaab365713099ffbde
Rajat0023 <rajatsingh2237@gmail.com>
2019-09-19 20:33:45 +0530

    Added methods for delete Address and getOrderByAddress

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/AddressDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/AddressDao.java
@@ -2,6 +2,8 @@ package com.upgrad.FoodOrderingApp.service.dao;
 
 
 import com.upgrad.FoodOrderingApp.service.entity.AddressEntity;
+import com.upgrad.FoodOrderingApp.service.entity.CustomerAddress;
+import com.upgrad.FoodOrderingApp.service.entity.Orders;
 import com.upgrad.FoodOrderingApp.service.entity.StateEntity;
 import org.springframework.stereotype.Repository;
 
@@ -28,5 +30,36 @@ public class AddressDao {
     public AddressEntity createAddress(AddressEntity address) {
         entityManager.persist(address);
         return address;
+    }
+
+    public AddressEntity getAddressByUuid(String addressUuid) {
+        try {
+            return entityManager.createNamedQuery("findAddressByUUID", AddressEntity.class)
+                    .setParameter("uuid",addressUuid)
+                    .getSingleResult();
+        }
+        catch (NoResultException nre) {
+            return null;
+        }
+    }
+
+    public void createCustomerAddress(CustomerAddress customerAddress) {
+        entityManager.persist(customerAddress);
+    }
+
+    public AddressEntity deleteAddress(AddressEntity addressEntity) {
+       entityManager.remove(addressEntity);
+        return addressEntity;
+    }
+
+    public Orders getOrderByAddressId(AddressEntity addressEntity) {
+        try {
+            return entityManager.createNamedQuery("findOrderByAddressId", Orders.class)
+                    .setParameter("addressId",addressEntity.getId())
+                    .getSingleResult();
+        }
+        catch (NoResultException nre) {
+            return null;
+        }
     }
 }
