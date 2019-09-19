c39b8e63a6345883784b7da8d0467a6db653554f
Rajat0023 <rajatsingh2237@gmail.com>
2019-09-19 20:32:51 +0530

    Changed Mapping b/w Address and state as ManyToOne & removed Delete Cascade

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/AddressEntity.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/AddressEntity.java
@@ -7,90 +7,118 @@ import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "address")
-
+@NamedQueries({
+  @NamedQuery(
+      name = "findAddressByUUID",
+      query = "Select a from AddressEntity a where a.uuid = :uuid")
+})
 public class AddressEntity {
 
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer Id;
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer Id;
 
-    @Column(name= "uuid")
-    @NotNull
-    @Size(max = 200)
-    private String uuid;
+  @Column(name = "uuid")
+  @NotNull
+  @Size(max = 200)
+  private String uuid;
 
-    @Column(name="flat_buil_number")
-    @Size(max = 255)
-    private String flatNumber;
+  @Column(name = "flat_buil_number")
+  @Size(max = 255)
+  private String flatNumber;
 
-    @Column(name="locality")
-    @Size(max = 255)
-    private String locality;
+  @Column(name = "locality")
+  @Size(max = 255)
+  private String locality;
 
-    @Column(name="city")
-    @Size(max = 30)
-    private String city;
+  @Column(name = "city")
+  @Size(max = 30)
+  private String city;
 
-    @Column(name="pincode")
-    @Size(max = 30)
-    private String pinCode;
+  @Column(name = "pincode")
+  @Size(max = 30)
+  private String pinCode;
 
-    @OneToOne(cascade = CascadeType.REMOVE)
-    @JoinColumn(name = "state_id",referencedColumnName = "id")
-    private StateEntity state;
+  @ManyToOne
+  @JoinColumn(name = "state_id", referencedColumnName = "id")
+  private StateEntity state;
 
+  @Column(name = "active")
+  private Integer active;
 
-    public Integer getId() {
-        return Id;
+  @OneToOne(mappedBy = "address")
+  private CustomerAddress customerAddress;
+
+  public Integer getId() {
+    return Id;
+  }
+
+  public void setId(Integer id) {
+    Id = id;
+  }
+
+  public String getUuid() {
+    return uuid;
+  }
+
+  public void setUuid(String uuid) {
+    this.uuid = uuid;
+  }
+
+  public String getFlatNumber() {
+    return flatNumber;
+  }
+
+  public void setFlatNumber(String flatNumber) {
+    this.flatNumber = flatNumber;
+  }
+
+  public String getLocality() {
+    return locality;
+  }
+
+  public void setLocality(String locality) {
+    this.locality = locality;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public String getPinCode() {
+    return pinCode;
+  }
+
+  public void setPinCode(String pinCode) {
+    this.pinCode = pinCode;
+  }
+
+  public StateEntity getState() {
+    return state;
+  }
+
+  public void setState(StateEntity state) {
+    this.state = state;
+  }
+
+  public CustomerAddress getCustomerAddress() {
+    return customerAddress;
+  }
+
+  public void setCustomerAddress(CustomerAddress customerAddress) {
+    this.customerAddress = customerAddress;
+  }
+
+  public Integer isActive() {
+    return active;
+  }
+
+  public void setActive(Integer active) {
+    this.active = active;
+  }
     }
-
-    public void setId(Integer id) {
-        Id = id;
-    }
-
-    public String getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
-    public String getFlatNumber() {
-        return flatNumber;
-    }
-
-    public void setFlatNumber(String flatNumber) {
-        this.flatNumber = flatNumber;
-    }
-
-    public String getLocality() {
-        return locality;
-    }
-
-    public void setLocality(String locality) {
-        this.locality = locality;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getPinCode() {
-        return pinCode;
-    }
-
-    public void setPinCode(String pinCode) {
-        this.pinCode = pinCode;
-    }
-
-    public StateEntity getState() { return state; }
-
-    public void setState(StateEntity state) { this.state = state; }
-
-}
